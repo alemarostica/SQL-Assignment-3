d64829fb47042e2f6a57845ba0e5863169a5d8e4
alemarostica <alemarostica@virgilio.it>
2022-12-14 19:32:59 +0100

    connection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:postgresql://sci-didattica.unitn.it/db_170";
+
+        Properties props = new Properties();
+        props.setProperty("user", "db_170");
+        props.setProperty("password", "pass_170");
+        props.setProperty("ssl", "false");
+
+        Connection db = DriverManager.getConnection(url, props);
+        System.out.println("Connected");
+
+        db.close();
     }
 }
