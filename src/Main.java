28bd34500504fc1c4d8b30dd12ce050a8882fddd
alemarostica <alemarostica@virgilio.it>
2022-12-16 00:52:10 +0100

    Manca 8

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
 import java.sql.*;
+import java.util.*;
 import java.util.Date;
-import java.util.Properties;
+
 
 public class Main {
+
+    static int idn = 1;
+    static class Professor {
+        int id;
+        String name;
+        String address;
+        int age;
+        float department;
+
+        public Professor() {
+            Random rnd = new Random();
+            this.id = idn++;
+            this.name = UUID.randomUUID().toString().substring(0, 36);
+            this.address = UUID.randomUUID().toString().substring(0, 36);
+            this.age = rnd.nextInt(18,100);
+            this.department = rnd.nextInt(1000000000);
+        }
+    }
+
+    static class Course {
+        String cid;
+        String cname;
+        String credits;
+        int teacher;
+
+        public Course(){
+            Random rnd = new Random();
+            this.cid = UUID.randomUUID().toString().substring(0, 24);
+            this.cname = UUID.randomUUID().toString().substring(0, 36);
+            this.credits = UUID.randomUUID().toString().substring(0, 29);
+        }
+    }
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:postgresql://sci-didattica.unitn.it/db_170";
 
@@ -15,7 +48,12 @@ public class Main {
         System.out.println("Connected");
 
         Statement stmt;
+        PreparedStatement pst;
         int time1, time2;
+        int count;
+        Random rnd = new Random();
+
+
 
         //ex1
         time1 = (int) new Date().getTime();
@@ -29,86 +67,176 @@ public class Main {
         //ex2
         time1 = (int) new Date().getTime();
         stmt = db.createStatement();
-        String sqlCreTab = "CREATE TABLE student " +
+        String sqlCreTab = "CREATE TABLE professor " +
                 "(id INTEGER not NULL, " +
                 "name VARCHAR(50), " +
                 "address VARCHAR(50), " +
-                "birthplace INTEGER not NULL, " +
+                "age INTEGER not NULL, " +
                 "department FLOAT not NULL," +
                 "PRIMARY KEY (id));" +
                 "CREATE TABLE course " +
-                "(cid INTEGER not NULL, " +
+                "(cid VARCHAR(25), " +
                 "cname VARCHAR(50), " +
                 "credits VARCHAR(30), " +
                 "teacher INTEGER not NULL, " +
-                "PRIMARY KEY (cid))";
+                "PRIMARY KEY (cid), " +
+                "FOREIGN KEY (teacher) REFERENCES professor(id));";
         stmt.executeUpdate(sqlCreTab);
         time2 = (int) new Date().getTime();
-        System.out.println("Step 1 needs " + (time2 - time1) + " ns");
+        System.out.println("Step 2 needs " + (time2 - time1) + " ns");
         stmt.close();
 
         //ex3
+        time1 = (int) new Date().getTime();
+        String query1 = "INSERT INTO professor (id, name, address, age, department) " +
+                "VALUES (?, ?, ?, ?, ?);";
+        pst = db.prepareStatement(query1);
 
-        /*Statement stmt = db.createStatement();
-
-        String sqlCheck = "DROP TABLE IF EXISTS student";
-        String sql = "CREATE TABLE student"
-                + "(id INTEGER not NULL, "
-                + "name VARCHAR(255), "
-                + "surname VARCHAR(255), "
-                + "birthplace VARCHAR(255), "
-                + "PRIMARY KEY (id));";
-
-        stmt.executeUpdate(sqlCheck);
-        stmt.executeUpdate(sql);
-
-        System.out.println("Table created");*/
-
-        /*String sqlInsert = "INSERT INTO student (id, name, surname, birthplace) "
-                + "VALUES (183475, 'Mario', 'Rossi', 'Trento');";
-        stmt.executeUpdate(sqlInsert);
-        System.out.println("Added");*/
-
-        /*String query = "INSERT INTO student (id, name, surname, birthplace) "
-                + "VALUES (?, ?, ?, ?);";
-        PreparedStatement st = db.prepareStatement(query);
-        st.setObject(1, 123456);
-        st.setObject(2, "Marco");
-        st.setObject(3, "Sbizzera");
-        st.setObject(4, "Napoli");
-        st.executeUpdate();
-        st.close();*/
-
-        /*Statement st = db.createStatement();
-        ResultSet rs = st.executeQuery(
-                "SELECT * FROM student;"
-        );
-
-        while(rs.next()){
-            System.out.println("ID: " + rs.getInt(1));
-            System.out.println("Name: " + rs.getString("name"));
-            System.out.println("Surname: " + rs.getString("surname"));
-            System.out.println("Birthplace: " + rs.getString(4) + '\n');
+        ArrayList<Professor> list = new ArrayList<Professor>();
+        for(int i = 0; i < 1000; i++){
+            list.add(new Professor());
         }
 
-        rs.close();
-        st.close();
-        */
+        count = 0;
+        for(Professor prof : list){
+            pst.setObject(1, prof.id);
+            pst.setString(2, prof.name);
+            pst.setString(3, prof.address);
+            pst.setObject(4, prof.age);
+            if(count != 999) pst.setObject(5, prof.department);
+            else pst.setObject(5, 1940);
 
-        /*String surname = "Neri";
-        PreparedStatement pst = db.prepareStatement(
-                "SELECT * FROM student " +
-                        "WHERE surname = ?;"
+            pst.addBatch();
+            count++;
+
+            if(count % 100 == 0 || count == list.size()){
+                pst.executeBatch();
+            }
+        }
+
+        time2 = (int) new Date().getTime();
+        System.out.println("Step 3 needs " + (time2 - time1) + " ns");
+        pst.close();
+
+        //ex4
+        time1 = (int) new Date().getTime();
+        String query2 = "INSERT INTO course (cid, cname, credits, teacher) " +
+                "VALUES (?, ?, ?, ?);";
+        pst = db.prepareStatement(query2);
+
+        ArrayList<Course> clist = new ArrayList<Course>();
+        for(int i = 0; i < 1000; i++){
+            clist.add(new Course());
+        }
+
+        count = 0;
+        for(Course c : clist){
+            pst.setString(1, c.cid);
+            pst.setString(2, c.cname);
+            pst.setString(3, c.credits);
+            pst.setObject(4, list.get(rnd.nextInt(list.size())).id);
+
+            pst.addBatch();
+            count++;
+
+            if(count % 100 == 0 || count == clist.size()){
+                pst.executeBatch();
+            }
+        }
+
+        time2 = (int) new Date().getTime();
+        System.out.println("Step 4 needs " + (time2 - time1) + " ns");
+        pst.close();
+
+        //ex5
+        time1 = (int) new Date().getTime();
+        stmt = db.createStatement();
+        ResultSet rsp = stmt.executeQuery(
+                "SELECT id FROM professor;"
         );
 
-        pst.setString(1, surname);
-        ResultSet rs = pst.executeQuery();
-        while(rs.next()){
-            System.out.println("ID: " + rs.getInt(1));
-            System.out.println("Name: " + rs.getString("name"));
-            System.out.println("Surname: " + rs.getString("surname"));
-            System.out.println("Birthplace: " + rs.getString(4) + '\n');
-        }*/
+        while(rsp.next()){
+            System.err.println(rsp.getInt(1));
+        }
+
+        time2 = (int) new Date().getTime();
+        System.out.println("Step 5 needs " + (time2 - time1) + " ns");
+        stmt.close();
+
+        //ex6
+        time1 = (int) new Date().getTime();
+        stmt = db.createStatement();
+        String updateDep = "UPDATE professor " +
+                "SET department = 1973 " +
+                "WHERE department = 1940;";
+        stmt.executeUpdate(updateDep);
+
+        time2 = (int) new Date().getTime();
+        System.out.println("Step 6 needs " + (time2 - time1) + " ns");
+        stmt.close();
+
+        //ex7
+        time1 = (int) new Date().getTime();
+        stmt = db.createStatement();
+        ResultSet rsp2 = stmt.executeQuery(
+                "SELECT id, address FROM professor " +
+                        "WHERE department = 1973;"
+        );
+
+        while(rsp2.next()){
+            System.err.print("ID: " + rsp2.getInt(1) + ", ");
+            System.err.println("Dep: " + rsp2.getString(2));
+        }
+
+        time2 = (int) new Date().getTime();
+        System.out.println("Step 7 needs " + (time2 - time1) + " ns");
+        stmt.close();
+
+        //ex8
+
+        //ex9
+        time1 = (int) new Date().getTime();
+        stmt = db.createStatement();
+        ResultSet rsp3 = stmt.executeQuery(
+                "SELECT id FROM professor;"
+        );
+
+        while(rsp3.next()){
+            System.err.println(rsp3.getInt(1));
+        }
+
+        time2 = (int) new Date().getTime();
+        System.out.println("Step 9 needs " + (time2 - time1) + " ns");
+        stmt.close();
+
+        //ex10
+        time1 = (int) new Date().getTime();
+        stmt = db.createStatement();
+        String updateDep2 = "UPDATE professor " +
+                "SET department = 1974 " +
+                "WHERE department = 1973;";
+        stmt.executeUpdate(updateDep2);
+
+        time2 = (int) new Date().getTime();
+        System.out.println("Step 10 needs " + (time2 - time1) + " ns");
+        stmt.close();
+
+        //ex11
+        time1 = (int) new Date().getTime();
+        stmt = db.createStatement();
+        ResultSet rsp4 = stmt.executeQuery(
+                "SELECT id, address FROM professor " +
+                        "WHERE department = 1974;"
+        );
+
+        while(rsp4.next()){
+            System.err.print("ID: " + rsp4.getInt(1) + ", ");
+            System.err.println("Dep: " + rsp4.getString(2));
+        }
+
+        time2 = (int) new Date().getTime();
+        System.out.println("Step 11 needs " + (time2 - time1) + " ns");
+        stmt.close();
 
         db.close();
     }
